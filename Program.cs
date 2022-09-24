c3fefc46ed363392228463d415e5ee874fa82cae
HazemLenin <63303495+HazemLenin@users.noreply.github.com>
2022-09-24 17:52:39 +0200

    first trial

--- Program.cs
+++ Program.cs
@@ -9,8 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var posgresqlConnectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    // options.UseSqlServer(connectionString)
+    options.UseNpgsql(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
